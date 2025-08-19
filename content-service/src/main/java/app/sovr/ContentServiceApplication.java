fa8711b185f5325bac960bd84939b97a2dffcdde
macwlu97 <maciej.wnuklipinski@gmail.com>
2025-08-19 19:49:11 +0200

    init

---
+++ content-service/src/main/java/app/sovr/ContentServiceApplication.java
@@ -0,0 +1,13 @@
+package app.learnopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ContentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentServiceApplication.class, args);
+    }
+}
